4e390010efb4df7b232645291adad8ff87e8f7f2
tusharanilmalali <tusharnil@hyperface.co>
2026-01-08 21:58:10 +0530

    Created data repositories using @Repository annotation for all three entity classes

---
+++ src/main/java/com/company/company/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.company.company.repository;
+
+import com.company.company.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
